a026f7ac6827be65648c1610cc88a6a59d1f912f
YasiruNB <155614875+ynbdissanayake@users.noreply.github.com>
2025-04-26 11:36:44 +0530

    Exception Handling.

---
+++ backend/src/main/java/backend/exception/CommunityNotFoundException.java
@@ -0,0 +1,7 @@
+package backend.exception;
+
+public class CommunityNotFoundException extends RuntimeException {
+    public CommunityNotFoundException(String message) {
+        super(message);
+    }
+}
